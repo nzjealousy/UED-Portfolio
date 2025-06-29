f8a5a50061a1dad2e055789748e47f2652968a81
nzjealousy <103101806+nzjealousy@users.noreply.github.com>
2025-06-29 14:32:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//menu
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+//Collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
